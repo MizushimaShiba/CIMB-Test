64a918f148b87be755f6b2e5d499b372f735895e
水島芝 <haidarme@yahoo.co.id>
2023-09-14 17:04:59 +0700

    Initial commit

---
+++ src/main/java/com/test/CIMBTest/CimbTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.CIMBTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CimbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CimbTestApplication.class, args);
+	}
+
+}
